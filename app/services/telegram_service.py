226aa7482dd894a26da1a1700cb34d97a01f1c1a
leonwu127 <leonwu127@gmail.com>
2024-05-15 23:45:10 +0100

    add init files

--- app/services/telegram_service.py
+++ app/services/telegram_service.py
@@ -2,12 +2,15 @@
 
 from telegram import Bot
 from config import settings
+from app.utils.logging import get_logger
+
+logger = get_logger(__name__)
 
 bot = Bot(token=settings.TELEGRAM_TOKEN)
-
 
 def start_bot_polling():
     from telegram.ext import Updater
     updater = Updater(token=settings.TELEGRAM_TOKEN, use_context=True)
     updater.start_polling()
     updater.idle()
+    logger.info("Telegram bot polling started")
