275199402e652a0cbceeb272718f6ad8f9049a3a
leonwu127 <leonwu127@gmail.com>
2024-05-23 01:19:04 +0100

    add user collection save  action to login

--- app/api/endpoints/telegram.py
+++ app/api/endpoints/telegram.py
@@ -1,13 +1,33 @@
 # app/api/endpoints/telegram.py
-
-from fastapi import APIRouter
-from app.models.schemas import TelegramLoginRequest
+from fastapi import APIRouter, HTTPException
+from pymongo import MongoClient
+from pydantic import ValidationError
+from app.models.schemas import TelegramLoginRequest, User
 
 router = APIRouter()
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['rabbitDB']
+users = db['users']
 
 @router.post("/login")
 async def telegram_login(request: TelegramLoginRequest):
-    user = request.dict()
-    # Here you would normally check the database and create/update the user record
-    return user
+    try:
+        login = TelegramLoginRequest(**request.dict())
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    existing_user = users.find_one({"tg_id": login.tg_id})
+    if existing_user is not None:
+        return
+
+    user = User(
+        tg_id=login.tg_id,
+        first_name=login.first_name,
+        last_name=login.last_name,
+        username=login.username,
+        language_code=telegram_login.language_code
+    )
+    users.insert_one(user.dict())
+
+    return user.dict()
