cdc1a5b5a46a6db57cfb31e3ea2ab6982d585299
leonwu127 <leonwu127@gmail.com>
2024-06-01 22:19:16 +0100

    add mongo password w/ connection string

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ class Settings:
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
     WELCOME_MESSAGE: str = os.getenv("WELCOME_MESSAGE")
+    MONGO_URI: str = os.getenv("MONGO_URI")
 
 settings = Settings()
