3ee87456ffd24326a258a697cf151b8c129b7ff5
leonwu127 <leonwu127@gmail.com>
2024-06-02 05:19:43 +0100

    revert to old bot and update the mongo URI

--- config.py
+++ config.py
@@ -6,13 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-with open('config.json') as config_file:
+config_file_path = os.path.join(os.path.dirname(__file__), 'config.json')
+
+with open(config_file_path) as config_file:
     configs = json.load(config_file)
+
 
 class Settings:
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
     WELCOME_MESSAGE: str = configs["welcome_message"]
     MONGO_URI: str = os.getenv("MONGO_URI")
+
 
 settings = Settings()
