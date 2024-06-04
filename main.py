199810d2767635139b5b11805f2a4d8a4e854dfa
genggeng <ke.sun.ucl@gmail.com>
2024-06-04 22:39:57 +0800

    fixed referrer not imported

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import asyncio
 from fastapi import FastAPI
 
-from app.api.endpoints import fortune, telegram
+from app.api.endpoints import fortune, telegram, referrer
 from app.services.telegram_service import start_bot_polling
 from app.utils.logging import get_logger
 import uvicorn
@@ -25,7 +25,8 @@ logger = get_logger(__name__)
 # Include the routers for API endpoints
 app.include_router(fortune.router, prefix="/fortune", tags=["fortune"])
 app.include_router(telegram.router, prefix="/telegram", tags=["telegram"])
-app.include_router(telegram.router, prefix="/referral", tags=["referral"])
+app.include_router(referrer.router, prefix="/referral", tags=["referral"])
+
 
 async def start_bot():
     await start_bot_polling()
