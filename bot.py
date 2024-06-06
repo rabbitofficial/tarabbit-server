0c20bf6c81597fe75d31faa6444a15a45aa6b4aa
leonwu127 <leonwu127@gmail.com>
2024-06-06 02:34:57 +0100

    add all files and seperate two services

--- bot.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from app.services.telegram_service import start_bot_polling
-
-async def start_bot():
-    await start_bot_polling()
-    
-if __name__ == "__main__":
-    asyncio.run(start_bot())
