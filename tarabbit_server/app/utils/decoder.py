2eb8a304f5afd5136f1811f00caff63b5500a64f
leonwu127 <leonwu127@gmail.com>
2024-06-15 16:04:24 +0100

    update decrpt function

--- tarabbit_server/app/utils/decoder.py
+++ tarabbit_server/app/utils/decoder.py
@@ -1,11 +1,7 @@
 from cryptography.fernet import Fernet
 
 class Decoder:
-    def __init__(self, secret_key, encrypted_message):
-        self.secret_key = secret_key
-        self.encrypted_message = encrypted_message
-        
-    def decrypt(self):
-        cipher_suite = Fernet(self.secret_key)
-        decrypted_message = cipher_suite.decrypt(self.encrypted_message)
+    def decrypt(secret_key, encrypted_message):
+        cipher_suite = Fernet(secret_key)
+        decrypted_message = cipher_suite.decrypt(encrypted_message)
         return decrypted_message.decode("utf-8")
